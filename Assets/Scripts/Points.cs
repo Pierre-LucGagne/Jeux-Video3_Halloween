316569720a6ca173f4ff0a80dbc1c249a025da14
AmelieCousineau <ameliecousineau5@gmail.com>
2023-11-01 08:38:59 -0400

    script point fini

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Points : MonoBehaviour
 {
+    private int nbPoint = 0;
+
+    [SerializeField] private TMP_Text champPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +18,13 @@ public class Points : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        champPoint.text = nbPoint.ToString();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "grab"){
+            nbPoint++;
+        }
     }
 }
